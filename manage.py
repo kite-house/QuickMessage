71b8004693e2faf60e12a95a1e63e19a2ab6b787
kite-house <hodosok1301@gmail.com>
2024-07-26 17:28:06 +0700

    Added calling processes such as GUi

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from os import getenv
 from telethon import TelegramClient, events
 from config.processingCommands import *
+from multiprocessing import Process
+from gui.main import launch
 
 client = TelegramClient('client', getenv('api_id'), getenv('api_hash'))
 
@@ -17,9 +19,14 @@ async def command_handler(message):
 @client.on(events.NewMessage(outgoing=True, pattern='/test'))
 async def output_interface(message):
     await message.delete()
-    #
+    GuiProcess = Process(target=launch, name = 'GuiProcess')
+    GuiProcess.start()
     
+def launch_telegram():
+    client.start()
+    client.run_until_disconnected()
 
-
-client.start()
-client.run_until_disconnected()
+if __name__ == '__main__':
+    MainProcess = Process(target=launch_telegram, name = 'TelegramPluginProcess')
+    MainProcess.start()
+    MainProcess.join()
